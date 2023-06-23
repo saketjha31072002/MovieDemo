07f672bac8b05da929ad13e90a3b352a16a2832d
Nitindev135 <devnitin83@gmail.com>
2023-06-23 12:27:48 +0530

    Fixed api path mapping

--- movie/src/main/java/com/movies/movie/repository/MovieRepository.java
+++ movie/src/main/java/com/movies/movie/repository/MovieRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.movies.movie.model.Movie;
-
 @Repository
 public interface MovieRepository extends JpaRepository<Movie,Long> {
     
